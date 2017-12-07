5d982212bbabe4afe9ec14ffa453ac36af227155
Patrick McConnell <pmcconnell@godaddy.com>
2017-12-06 21:25:50 -0600

    commit from new laptop

--- tests/park_tests.py
+++ tests/park_tests.py
@@ -3,9 +3,10 @@ from mock import patch, MagicMock
 from nose.tools import assert_true
 
 
-class TestParked():
+class TestParked:
 
     def __init__(self):
         self._park = Parked()
 
     def test_is_parked(self):
+        pass
