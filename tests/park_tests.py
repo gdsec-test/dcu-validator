7ccda02b19698b47d36bdb85f47179de19c90600
Patrick McConnell <pmcconnell@godaddy.com>
2017-12-08 08:23:34 -0800

    Parked class returns true (is parked or suspended) or false (is not parked or suspended) Tests coverage for parked.py 100%

--- tests/park_tests.py
+++ tests/park_tests.py
@@ -1,13 +1,53 @@
 from checks.parked import Parked
-from mock import patch, MagicMock
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_false
 
 
 class TestParked:
 
     def __init__(self):
         self._park = Parked()
-        self.domain = 'comicsn.tech'
 
-    def test_is_parked(self):
-        pass
+    def test_is_parked_domain_ip(self):
+        domain = 'comicsn.tech'
+        content = ''
+        url = 'http://comicsn.tech/test.html'
+
+        result = self._park.is_parked(domain, content, url)
+
+        return assert_true(result)
+
+    def test_is_parked_regex(self):
+        domain = 'comicsn.beer'
+        content = 'OMG, its a Future home of something quite cool!!!!1!!1!!! Coming Soon'
+        url = 'http://comicsn.beer/test.html'
+
+        result = self._park.is_parked(domain, content, url)
+
+        return assert_true(result)
+
+    def test_is_parked_suspended(self):
+        domain = 'comicsn.beer'
+        content = ''
+        url = 'http://comicsn.beer/cgi-sys/suspendedpage.cgi'
+
+        result = self._park.is_parked(domain, content, url)
+
+        return assert_true(result)
+
+    def test_is_parked_false(self):
+        domain = 'comicsn.beer'
+        content = 'just a website'
+        url = 'http://comicsn.beer/index.php'
+
+        result = self._park.is_parked(domain, content, url)
+
+        return assert_false(result)
+
+    def test_is_parked_ip_ip(self):
+        domain = '184.168.221.47'
+        content = ''
+        url = '184.168.221.47/index.php'
+
+        result = self._park.is_parked(domain, content, url)
+
+        return assert_true(result)
