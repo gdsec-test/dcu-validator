f3fbe98e44eadf85febd731ac9d301b670a97c21
Chris Hauser <chauser@godaddy.com>
2017-12-08 13:22:33 -0700

    fixed test bug

--- rest/tests/api_tests.py
+++ rest/tests/api_tests.py
@@ -14,64 +14,64 @@ class TestValidator(TestCase):
     def setUp(self):
         self.client = self.app.test_client()
 
-    @patch('api.api.AddSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.AddSchedule')
+    @patch('rest.api.api.service_connect')
     def test_schedule_success(self, service, add):
         data = dict(period=300)
         response = self.client.post(url_for('scheduler', ticketid=12345), data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 201)
 
-    @patch('api.api.AddSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.AddSchedule')
+    @patch('rest.api.api.service_connect')
     def test_reschedule_success(self, service, add):
         data = dict(period=300)
         response = self.client.post(url_for('scheduler', ticketid=12345), data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 201)
 
-    @patch('api.api.AddSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.AddSchedule')
+    @patch('rest.api.api.service_connect')
     def test_schedule_max_fail(self, service, add):
         data = dict(period=3000000000)
         response = self.client.post(url_for('scheduler', ticketid=12345), data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 400)
 
-    @patch('api.api.AddSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.AddSchedule')
+    @patch('rest.api.api.service_connect')
     def test_schedule_min_fail(self, service, add):
         data = dict(period=3)
         response = self.client.post(url_for('scheduler', ticketid=12345), data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 400)
 
-    @patch('api.api.AddSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.AddSchedule')
+    @patch('rest.api.api.service_connect')
     def test_schedule_no_close(self, service, add):
         data = dict(period=300, close=False)
         response = self.client.post(url_for('scheduler', ticketid=12345), data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 201)
 
-    @patch('api.api.AddSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.AddSchedule')
+    @patch('rest.api.api.service_connect')
     def test_schedule_invalid_close(self, service, add):
         data = dict(period=300, close=12345)
         response = self.client.post(url_for('scheduler', ticketid=12345), data=json.dumps(data), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 400)
 
-    @patch('api.api.RemoveSchedule')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.RemoveSchedule')
+    @patch('rest.api.api.service_connect')
     def test_remove(self, service, remove):
         response = self.client.delete(url_for('scheduler', ticketid=12345))
         self.assertEqual(response.status_code, 204)
 
-    @patch('api.api.ValidateTicket')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.ValidateTicket')
+    @patch('rest.api.api.service_connect')
     def test_validate_true(self, service, validate):
         response = self.client.get(url_for('validate', ticketid=12345))
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertTrue(data.get('valid'))
 
-    @patch('api.api.ValidateTicket')
-    @patch('api.api.service_connect')
+    @patch('rest.api.api.ValidateTicket')
+    @patch('rest.api.api.service_connect')
     def test_validate_false(self, service, validate):
         resp = namedtuple('ValidationResponse', 'valid')
         validate.return_value = resp(False)
