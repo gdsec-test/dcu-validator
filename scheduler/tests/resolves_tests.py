03ef9ef3f01a67ed81faf923baca1f0e6f1f20c0
abean <abean@godaddy.com>
2017-12-12 17:23:52 -0700

    updated resolves tests and requirements per PR comments

--- scheduler/tests/resolves_tests.py
+++ scheduler/tests/resolves_tests.py
@@ -1,30 +1,69 @@
+from mock import patch, MagicMock
 from nose.tools import assert_true, assert_false
+from requests.sessions import Session
 
 from scheduler.validators.resolves import Resolvable
 
 
 class TestResolvable:
 
+    url = 'http://comicsn.beer/'
+
     def __init__(self):
         self._resolvable = Resolvable()
 
-    def test_resolves(self):
-        url = 'http://comicsn.beer'
+    @patch.object(Session, 'request')
+    def test_resolves_1xx(self, request):
+        request.return_value = MagicMock(status_code=100)
 
-        result = self._resolvable.resolves(url)
+        result = self._resolvable.resolves(self.url)
 
         return assert_true(result)
 
-    def test_not_resolves(self):
-        url = 'http://comicsn.beer/puppies'
+    @patch.object(Session, 'request')
+    def test_resolves_2xx(self, request):
+        request.return_value = MagicMock(status_code=200)
 
-        result = self._resolvable.resolves(url)
+        result = self._resolvable.resolves(self.url)
+
+        return assert_true(result)
+
+    @patch.object(Session, 'request')
+    def test_resolves_3xx(self, request):
+        request.return_value = MagicMock(status_code=300)
+
+        result = self._resolvable.resolves(self.url)
+
+        return assert_true(result)
+
+    @patch.object(Session, 'request')
+    def test_resolves_403(self, request):
+        request.return_value = MagicMock(status_code=403)
+
+        result = self._resolvable.resolves(self.url)
+
+        return assert_true(result)
+
+    @patch.object(Session, 'request')
+    def test_resolves_406(self, request):
+        request.return_value = MagicMock(status_code=406)
+
+        result = self._resolvable.resolves(self.url)
+
+        return assert_true(result)
+
+    @patch.object(Session, 'request')
+    def test_not_resolves_404(self, request):
+        request.return_value = MagicMock(status_code=404)
+
+        result = self._resolvable.resolves(self.url)
 
         return assert_false(result)
 
-    def test_resolves_proxy(self):
-        url = 'http://comicsn.beer/puppies'
+    @patch.object(Session, 'request')
+    def test_not_resolves_proxy(self, request):
+        request.return_value = MagicMock(status_code=404)
 
-        result = self._resolvable.resolves(url, proxy=True)
+        result = self._resolvable.resolves(self.url, proxy=True)
 
         return assert_true(result)
