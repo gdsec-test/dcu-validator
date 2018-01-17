0b2e10145ac0ef45a5e5a32ec7fcf29f7d830e77
Patrick McConnell <pmcconnell@godaddy.com>
2018-01-17 12:50:41 -0600

    update per PR change request

--- scheduler/tests/dedicated_ip_tests.py
+++ scheduler/tests/dedicated_ip_tests.py
@@ -1,6 +1,5 @@
 from nose.tools import assert_equal
 from scheduler_service.validators.dedicated_ip import DedicatedIpValidator
-from scheduler_service.utils.ipam import Ipam
 from mock import patch, MagicMock
 
 
@@ -9,18 +8,18 @@ class TestDedicated:
     def __init__(self):
         self._dedicated = DedicatedIpValidator()
 
-    @patch('scheduler_service.utils.ipam.Ipam')
-    @patch.object(Ipam, 'get_properties_for_ip')
-    def test_is_dedicated_ip(self, get_properties_for_ip, ipam):
-        get_properties_for_ip.return_value = MagicMock(HostName='VEL4567')
+    @patch('scheduler_service.validators.dedicated_ip.Ipam', autospec=True)
+    def test_is_dedicated_ip(self, ipam):
+        ipamo = ipam.return_value
+        ipamo.get_properties_for_ip.return_value = MagicMock(HostName='VEL4567')
         ticket = {'sourceDomainOrIp': '104.238.65.160'}
         result = self._dedicated.validate_ticket(ticket)
         return assert_equal(result, (True, ))
 
-    @patch('scheduler_service.utils.ipam.Ipam')
-    @patch.object(Ipam, 'get_properties_for_ip')
-    def test_is_not_dedicated_ip(self, get_properties_for_ip, ipam):
-        get_properties_for_ip.return_value = MagicMock(HostName='p3plcpnl0940')
+    @patch('scheduler_service.validators.dedicated_ip.Ipam', autospec=True)
+    def test_is_not_dedicated_ip(self, ipam):
+        ipamo = ipam.return_value
+        ipamo.get_properties_for_ip.return_value = MagicMock(HostName='p3plcpnl0940')
         ticket = {'sourceDomainOrIp': '160.153.77.227'}
         result = self._dedicated.validate_ticket(ticket)
         return assert_equal(result, (False, 'shared ip'))
