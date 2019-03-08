b216be9a504518964b4afa24196ef59f9da338bd
Stephanie Petersen <spetersen@godaddy.com>
2019-03-08 09:20:43 -0800

    [CMAPT-2749] Corrected necessary imports removed from route.py when flake8 & isort were introduced (#33)

--- scheduler/scheduler_service/validators/route.py
+++ scheduler/scheduler_service/validators/route.py
@@ -1,4 +1,12 @@
+from dedicated_ip import DedicatedIpValidator
+from domain_status import DomainStatusValidator
+from not_parked import ParkedValidator
+from resolves import ResolvesValidator
 from validator_interface import ValidatorInterface
+
+
+"""Any class that defines handlers must be imported, these imports will appear unused
+but are necessary in order to provide classes that define handlers"""
 
 
 # Route tickets based on type
@@ -6,8 +14,7 @@ def route(ticket):
     print 'Validating:{}'.format(ticket.get('type'))
     handlers = ValidatorInterface.registry.get(ticket.get('type'))
     for clazz in handlers:
-        ret = clazz().validate_ticket(
-            ticket)  # construct class and run interface function
+        ret = clazz().validate_ticket(ticket)  # construct class and run interface function
         if not ret[0]:
             return ret
 
